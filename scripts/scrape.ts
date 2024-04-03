3a697867bc1a983c88c8d9fa31c0b7f7c06bf25b
Amrit Rai <iamamrit27@gmail.com>
2024-04-03 15:09:13 +0530

    add getLinks

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -4,6 +4,31 @@ import { encode } from 'gpt-3-encoder';
 
 const BASE_URL = 'https://blog.samaltman.com/';
 
-(async () => {
+const getLinks = async () => {
+    const html = await axios.get(`${BASE_URL}articles.html`);
+    const $ = cheerio.load(html.data);
+    const tables = $("table");
 
-})();
+    const linksArr: { url: string; title: string }[] = [];
+
+    tables.each((i, table) => {
+        if (i === 2) {
+            const links = $(table).find("a");
+            links.each((i, link) => {
+                const url = $(link).attr("href");
+                const title = $(link).text();
+
+                if (url && url.endsWith(".html")) {
+                    const linkObj = {
+                        url,
+                        title
+                    };
+
+                    linksArr.push(linkObj);
+                }
+            });
+        }
+    });
+
+    return linksArr;
+};
