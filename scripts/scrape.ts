40eedb83798e933f864d487a1fdf3fda011f86b7
Amrit Rai <iamamrit27@gmail.com>
2024-04-03 14:49:48 +0530

    add script for scrape, embeddings next

---
+++ scripts/scrape.ts
@@ -0,0 +1,9 @@
+import axois from 'axios';
+import * as cheerio from 'cheerio';
+import { encode } from 'gpt-3-encoder';
+
+const BASE_URL = 'https://blog.samaltman.com/';
+
+(async () => {
+
+})();
