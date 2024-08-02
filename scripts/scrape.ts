5ba009d421d7030cf2178b37390710a15527fa0d
Amrit Rai <iamamrit27@gmail.com>
2024-08-02 08:39:03 +0530

    scrape works

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -4,12 +4,30 @@ import { encode } from "gpt-3-encoder";
 
 const BASE_URL = "https://blog.samaltman.com"
 
-const getLinks = async () => {
+const getTitles = async () => {
     const html = await axios.get(BASE_URL); 
     const $ = Cheerio.load(html.data);
-    console.log(html);
-}
+    const postTitle = $("h2");
+    const linksArr: { url: string; title: string }[] = [];
+
+    postTitle.each((i, title) => {
+        const links = $(title).find("a");
+        links.each((i, link) => {
+            const url = $(link).attr("href");
+            if (url) {
+                const title = $(link).text();
+                const linkObj = {
+                    url,
+                    title
+                }
+                linksArr.push(linkObj);
+            }
+        });
+    })
+    return linksArr;
+};
 
 (async () => {
-    await getLinks();
+    const titles = await getTitles(); 
+    console.log(titles); 
 })();
